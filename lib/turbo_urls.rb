8f7eb93ccca34bbea0615ff57eb19f263ca0e8c8
Akira Matsuda <ronnie@dio.jp>
2016-01-15 05:57:09 +0900

    Light my fire!
    This way we would be able to rewrite newly created URL helper methods on the fly

--- lib/turbo_urls.rb
+++ lib/turbo_urls.rb
@@ -1,8 +1,20 @@
 require "turbo_urls/version"
 
 module TurboUrls
+  module Interceptor; end
   @cache = {}
   def self.cache() @cache end
+
+  def self.i_m_your_turbo_lover!
+    [Rails.application.routes.named_routes.path_helpers_module, Rails.application.routes.named_routes.url_helpers_module].each do |mod|
+      mod.prepend TurboUrls::Interceptor
+      def mod.method_added(meth)
+        TurboUrls::Interceptor.module_eval do
+          define_method meth, TurboUrls::CACHE_IF_NO_ARGUMENT
+        end
+      end
+    end
+  end
 
   CACHE_IF_NO_ARGUMENT = ->(*args) do
     return super(*args) if args.any?
