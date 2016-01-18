24472be38bd367fe8659fd033415a38ba7fe3a00
Akira Matsuda <ronnie@dio.jp>
2016-01-19 03:42:59 +0900

    Ensure that this gem reduces the number of method calls :arrow_heading_down:

--- test/benchmark_test.rb
+++ test/benchmark_test.rb
@@ -19,4 +19,26 @@ class BenchmarkTest < ActiveSupport::TestCase
     original_ips = job.full_report.data.first[:ips]
     assert_true original_ips < turbo_ips
   end
+
+  test 'methods calls are reduced' do
+    app = ActionDispatch::Integration::Session.new(Rails.application)
+    app.conferences_path  # warm up
+
+    called_methods = []
+    tp = TracePoint.new(:call, :c_call) {|t| called_methods << [t.defined_class, t.method_id]}
+    tp.enable
+    app.conferences_path
+    tp.disable
+    turbo_called_methods = called_methods
+
+    TurboUrls::Interceptor.module_eval { def conferences_path() super; end }
+
+    called_methods = []
+    tp.enable
+    app.conferences_path
+    tp.disable
+
+    original_called_methods = called_methods
+    assert_true turbo_called_methods.length < original_called_methods.length
+  end
 end
